99f17da5ce6b715af9b33007f3600ac2fc336587
lpollini <lpollini@student.42firenze.it>
2023-12-28 00:04:20 +0100

    def

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lpollini <lpollini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/24 12:37:38 by lpollini          #+#    #+#             */
-/*   Updated: 2023/12/27 23:53:11 by lpollini         ###   ########.fr       */
+/*   Updated: 2023/12/28 00:04:02 by lpollini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 
 int main(int argn, char *argv[])
 {
-	printf("called. %li\n", ft_atoi_base(argv[1], 16));
+	printf("%li\n", ft_atoi_base(argv[1], 16));
 	//printf("called. %s\n", ls1->next->content);
 }
